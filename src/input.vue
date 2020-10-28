dcd7da596470af98e2b306ff81db16ef6f8f3192
XXXXXie1997 <1416849251@qq.com>
2020-10-28 20:01:34 +0800

    添加input组件，开始开发

---
+++ src/input.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input type="text">
+    </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name:''
+  }
+</script>
+
+<style lang='scss' scoped>
+
+</style>
