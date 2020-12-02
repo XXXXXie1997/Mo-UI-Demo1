5b47f082737236deb2ba6ae94137da868be6c16f
XXXXXie1997 <1416849251@qq.com>
2020-12-02 16:53:48 +0800

    修改参数名

--- src/tabs.vue
+++ src/tabs.vue
@@ -36,11 +36,11 @@
     mounted() {
       this.$children.forEach((vm) => {
         if (vm.$options.name === 'MoTabsHead') {
-          vm.$children.forEach((item) => {
-            if (item.$options.name === 'MoTabsItem'
-              && item.$props.name === this.selected) {
-              console.log(item.$el)
-              this.eventBus.$emit('update:selected', this.selected,item)
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'MoTabsItem'
+              && childVm.$props.name === this.selected) {
+              console.log(childVm.$el)
+              this.eventBus.$emit('update:selected', this.selected,childVm)
             }
           })
         }
