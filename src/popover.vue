cb26e3de68212b9e217c4f07c234e7ad117ce406
XXXXXie1997 <1416849251@qq.com>
2020-12-03 10:40:26 +0800

    实现简单popover功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div v-if="visible" class="content-wrapper">
+    <div class="popover" @click.stop="xxx">
+        <div v-if="visible" class="content-wrapper" @click.stop>
             <slot name="content"></slot>
         </div>
         <slot></slot>
@@ -17,6 +17,15 @@
     methods: {
       xxx() {
         this.visible = !this.visible
+        if (this.visible === true) {
+          setTimeout(() => {
+            let eventHandler =()=>{
+              this.visible = false
+              document.removeEventListener('click', eventHandler)
+            }
+            document.addEventListener('click', eventHandler)
+          }, 0)
+        }
       }
     }
   }
@@ -28,10 +37,11 @@
         vertical-align: top;
         position: relative;
     }
-    .content-wrapper{
+
+    .content-wrapper {
         position: absolute;
         bottom: 100%;
         border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
     }
 </style>
