0f3ee136d12ccb84b0f1c2889df05327cfe51d12
XXXXXie1997 <1416849251@qq.com>
2020-12-05 10:31:11 +0800

    添加single选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script lang='ts'>
+    import Vue from 'vue'
 export default {
   name:'MoCollapse',
+  props:{
+    single:{
+      type:Boolean,
+      default:false
+    }
+  },
+  data(){
+    return{
+      eventBus:new Vue
+    }
+  },
+  provide(){
+    if(this.single){
+      return{
+        eventBus: this.eventBus
+      }
+    }
+  }
 }
 </script>
 
