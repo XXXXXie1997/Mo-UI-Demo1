dea4efde99096096b63ef4b67fb4b083897fe2cc
XXXXXie1997 <1416849251@qq.com>
2020-12-05 17:14:52 +0800

    开始使用vuepress制作官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '"摸" UI',
+  description: '一个为了摸得透彻而生的UI框架',
+  // themeConfig: {
+  //   // sidebar: [
+  //   //   '/',
+  //   //   '/install/',
+  //   // ]
+  // }
+}
